b87514d6c971d1416e5f3f16cd68c091d7672c24
Cpt.Markizi <Cpt.Markizi@gmail.com>
2023-04-28 11:58:30 +0300

    Седьмой коммит

---
+++ main.js
@@ -0,0 +1,106 @@
+// function one () {
+//     console.log('Первая функция')
+// }
+
+// one()
+
+// let fun1 = document.querySelector('.function-test-1')
+
+// fun1.ondblclick = function(){
+// console.log('anonimus')
+// } 
+
+// fun1.onclick = onemptied
+// function borsh(){
+//     Порезать лук
+//     Порезать овощи
+//     Поставить кастрюлю 
+//     Включить газ
+// }
+
+// Приготовить борщ()
+
+// let fun = document.querySelector('.function-test-1')
+// function one(){
+//     console.log('Консоль функции')
+//     return 777
+// }
+
+// fun1.onclick = one
+
+// let a=8
+// let b=9
+// let с=0
+
+// function mat() {
+//     с +=a*b
+//     console.log(с)
+// }
+// mat()
+
+
+// let a = 8;
+// let b = 9;
+
+// function mat(){
+//     console.log(a*b)
+//     return a*b
+// }
+
+// console.log(mat())
+
+
+// function mlt(x, y){
+//     return x + y
+// }
+// console.log(mlt(30, 70))
+
+// let a = 'Консоль';
+// // console.log(a)
+// function mlt(x, y){
+//     console.log(a)
+//     // let a = 10
+//     return x + y 
+// }
+// console.log(mlt(30, 70))
+
+ 
+// let fun1 = document.querySelector('.function-test-1')
+// fun1.ondblсlick = function(){
+//     console.log('Anonim')
+// }
+
+// let fun1 = document.querySelector('.function-test-1')
+// fun1.onclick =() =>{
+//     console.log('Anonim')
+// }
+
+// let a = () =>{
+//     return console.log(123);
+// }
+// a()               Это основной метод использований функции
+
+
+// let a = (a,b) =>{
+//     return console.log(a + b);
+// }
+// a(10, 20)          // пример с аргументами
+
+
+// let block = document.querySelector('.root') // поиск блока 
+// let btn = document.querySelector('.btn') // поиск кнопки
+
+// btn.onclick = function(){ // при нажатии на кнопку что-то происходит
+// block.classList.add('dark')
+// }
+
+
+// ДОМАШНЕЕ ЗАДАНИЕ:
+
+// function Test() {
+//     var a = "Оно работает"; 
+//     var output = document.getElementById("output"); 
+//     output.innerHTML = a;
+//   }
+
+
